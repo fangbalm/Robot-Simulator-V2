d1a477e3d3456c21cce5787ad248d53967f63c60
Lucius VanSlyke <lu.vanslyke@gmail.com>
2021-02-04 10:58:06 -0500

    work with simongit status git status '

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+console.log("Hello")
+
 document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
 
   // ADD CODE HERE!
+  const movesContainer = document.querySelector("#moves-container")
+  const roboButton = document.getElementById('move-button')
+  console.log(movesContainer)
+  console.log(roboButton)
+
+  
+  // console.log(moveTest)
+
+  document.addEventListener('keydown', event => {
+    if (event.key == "ArrowUp"){ 
+      const listTag = document.createElement("li")
+      console.log("You went up!")
+      movesContainer.append(listTag) 
+      listTag.innerText = "Up"
+   } else if (event.key == "ArrowDown") {
+    const listTag = document.createElement("li")
+       console.log("You went down!")
+       movesContainer.append(listTag) 
+       listTag.innerText = "Down"
+    }
+    // console.log(event.key)
+    // debugger
+  })
+
+  document.addEventListener("click", event => {
+    console.log(event)
+    // if(event.key.startsWith("Arrow"))
+    // const list = document.getElementsByTagName('li')
+    // list.remove()
+  })
 });
+
+
+
